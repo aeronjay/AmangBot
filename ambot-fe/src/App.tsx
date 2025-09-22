f25225e235b86a97b86b5bbf4be584d6cf010583
Aeron <bulataoaeronjay@gmail.com>
2025-09-22 11:23:19 +0800

    add tailwind css

--- ambot-fe/src/App.tsx
+++ ambot-fe/src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import './index.css'
 
 function App() {
 
   return (
     <>
-      <div>
+      <div className='text-3xl font-bold underline'>
         ambot
       </div>
     </>
