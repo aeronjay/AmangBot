39efc66b368e6cb8e85571c0f4b7b0994e20e33a
Aeron <bulataoaeronjay@gmail.com>
2025-11-26 17:34:44 +0800

    add file

--- backend2/app.py
+++ backend2/app.py
@@ -16,7 +16,7 @@ from routes.auth import router as auth_router
 
 # --- Configuration ---
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATASET_DIR = os.path.abspath(os.path.join(BASE_DIR, "./Dataset/Integrated"))
+DATASET_DIR = os.path.abspath(os.path.join(BASE_DIR, "./Dataset/Integrated/Latest"))
 MODEL_PATH = os.path.abspath(os.path.join(BASE_DIR, "../Models/mistral-7b-instruct-v0.1.Q4_K_M.gguf"))
 BART_MODEL_PATH = os.path.abspath(os.path.join(BASE_DIR, "../Models/my_bart_model/"))
 NOMIC_MODEL_PATH = "../Models/nomic-finetuned/nomic-finetuned-final"
